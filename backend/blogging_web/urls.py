719db6641c7ffe9c97e6edb19608d8d34ab29560
Anurag Singh Tomar <anuraagsingh4545@gmail.com>
2026-03-24 00:41:52 +0530

    urls file updated

--- backend/blogging_web/urls.py
+++ backend/blogging_web/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Backend is running")),
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
 
